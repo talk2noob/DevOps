baa42d54b5e564bec2f304d87fdfab18092c6dc4
Mahesh Bheema <Mahesh_Bheema@epam.com>
2017-05-09 14:34:11 +0530

    daasd

--- version.ps1
+++ version.ps1
@@ -1,8 +1,9 @@
 $build= [int32](((get-date).Year-2000)*366)+(Get-Date).DayOfYear
+
 $Major_Revision = $env:majorrev
 $Minor_Revision = $env:minorrev
 $Build_Id = $env:BUILD_NUMBER
+
 $VersionNumber1 = $Major_Revision + '.' + $Minor_Revision + '.' + $build + '.' + $Build_Id
-cd %branchname%
+cd $env:project
 mvn -e versions:set -DnewVersion="$VersionNumber1"
-mvn $env:goal
